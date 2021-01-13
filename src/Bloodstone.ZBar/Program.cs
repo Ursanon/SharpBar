18eea5f7e4794a3a3544c7403f6536cda35bea08
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2021-01-13 19:56:28 +0100

    Moved to class

--- src/Bloodstone.ZBar/Program.cs
+++ src/Bloodstone.ZBar/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Bloodstone.ZBar
@@ -9,77 +7,19 @@ namespace Bloodstone.ZBar
     {
         public static async Task Main()
         {
-            var width = Console.BufferWidth;
-            var height = Console.BufferHeight;
+            var length = 10ul;
 
-            var length = 10;
-            var (left, top) = Console.GetCursorPosition();
-
-            var isLastLine = top == height - 1;
-            if (isLastLine)
+            using (var progress = new ProgressBar(length))
             {
-                --top;
+                for (var i = 0ul; i < length; ++i)
+                {
+                    await DoStuff();
+
+                    progress.RaportProgress();
+                }
             }
-
-            Console.CursorVisible = false;
-
-            var message = new StringBuilder();
-            var sw = new Stopwatch();
-            var lastElapsed = sw.Elapsed;
-            for (var i = 0; i < length; ++i)
-            {
-                message.Clear();
-
-                sw.Restart();
-                await DoStuff();
-                sw.Stop();
-
-                Console.SetCursorPosition(left, top);
-                var progress = (float)i / (length - 1);
-
-                if (isLastLine)
-                {
-                    message.Append(Environment.NewLine);
-                }
-
-                message.Append('[');
-                var remaining = sw.Elapsed * (length - 1 - i);
-                var progressStr = $" [{progress:P2}|{CalculateTotalTime(sw.Elapsed)}/it|{CalculateTotalTime(remaining)}]";
-                var offset = 2 + progressStr.Length;
-
-                var p = Math.Floor(progress * (width - offset));
-
-                for (var j = 0; j < p; ++j)
-                {
-                    message.Append((char)9608);
-                }
-
-                for (var j = p; j < (width - offset); ++j)
-                {
-                    message.Append('-');
-                }
-
-                message.Append(']')
-                       .Append(progressStr);
-
-                Console.WriteLine(message);
-
-                lastElapsed = sw.Elapsed;
-            }
-
-            Console.CursorVisible = true;
 
             await Task.CompletedTask;
-        }
-
-        private static string CalculateTotalTime(TimeSpan ts)
-        {
-            return ts.TotalMilliseconds switch
-            {
-                >= 100 => $"{ts.TotalSeconds:F2} s",
-                < 100 => $"{ts.TotalMilliseconds:F2} ms",
-                _ => throw new InvalidOperationException()
-            };
         }
 
         private static async Task DoStuff()
---
+++ src/Bloodstone.ZBar/ProgressBar.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Bloodstone.ZBar
+{
+    public sealed class ProgressBar : IDisposable
+    {
+        private readonly int _top;
+        private readonly int _left;
+        private readonly int _width;
+        private readonly int _height;
+        private readonly ulong _length;
+        private readonly Stopwatch _stopwatch;
+        private readonly StringBuilder _message;
+
+        private ulong _current = 0;
+
+        public ProgressBar(ulong length)
+        {
+            _length = length;
+            _stopwatch = new Stopwatch();
+            _message = new StringBuilder();
+
+            Console.CursorVisible = false;
+
+            _width = Console.BufferWidth;
+            _height = Console.BufferHeight;
+
+            (_left, _top) = Console.GetCursorPosition();
+
+            var isLastLine = _top == _height - 1;
+            if (isLastLine)
+            {
+                --_top;
+                _message.Append(Environment.NewLine);
+            }
+
+            RaportProgress();
+        }
+
+        public void RaportProgress()
+        {
+            Console.SetCursorPosition(_left, _top);
+            var progress = (float)_current / (_length);
+
+            _message.Append('[');
+            var remaining = _stopwatch.Elapsed * (_length - _current);
+            var progressStr = $" [{progress:P2}|{CalculateTotalTime(_stopwatch.Elapsed)}/it|{CalculateTotalTime(remaining)}]";
+            var offset = 2 + progressStr.Length;
+
+            var emptyWidth = Math.Floor(progress * (_width - offset));
+
+            for (var j = 0; j < emptyWidth; ++j)
+            {
+                _message.Append((char)9608);
+            }
+
+            for (var j = emptyWidth; j < (_width - offset); ++j)
+            {
+                _message.Append('-');
+            }
+
+            _message.Append(']')
+                    .Append(progressStr);
+
+            Console.WriteLine(_message);
+
+            _current++;
+            _message.Clear();
+
+            _stopwatch.Restart();
+        }
+
+        public void Dispose()
+        {
+            Console.CursorVisible = true;
+        }
+
+        private static string CalculateTotalTime(TimeSpan ts)
+        {
+            return ts.TotalMilliseconds switch
+            {
+                >= 100 => $"{ts.TotalSeconds:F2} s",
+                < 100 => $"{ts.TotalMilliseconds:F2} ms",
+                _ => throw new InvalidOperationException()
+            };
+        }
+    }
+}
